861988f3f56ea9980d22d17c3834c79e66b56519
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-12 20:07:51 +0530

    update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
+  imports: [DatabaseModule],
 })
 export class UserModule {}
