64ce67e80755adb9217e86e26898b0e3d67d7974
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-13 11:33:36 +0530

    update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { LocalAuthGuard } from 'src/auth/guards';
+import { GetUser, UseRole } from 'src/auth/decorators';
+import { Role } from './enum';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly _userService: UserService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Get()
+  public async getAllUsers(): Promise<any> {
+    return this._userService.getAllUsers();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Get(':userId')
+  public async getMe(@Param('userId') userId: string): Promise<any> {
+    return this._userService.getUserById(userId);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Get('admins')
+  public async getAllAdmins(): Promise<any> {
+    return this._userService.getAllAdmins();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Get('approvers')
+  public async getAllApprovers(): Promise<any> {
+    return this._userService.getAllApprovers();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Patch('grant/admin/:userId')
+  public async makeAdmin(@GetUser('id') userId: string): Promise<any> {
+    return this._userService.makeAdmin(userId);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Patch('revoke/admin/:userId')
+  public async revokeAdmin(@GetUser('id') userId: string): Promise<any> {
+    return this._userService.removeAdmin(userId);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Patch('make/approver/:userId')
+  public async makeApprover(@GetUser('id') userId: string): Promise<any> {
+    return this._userService.makeApprover(userId);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @UseRole(Role.ADMIN)
+  @Patch('remove/approver/:userId')
+  public async removeApprover(@GetUser('id') userId: string): Promise<any> {
+    return this._userService.removeApprover(userId);
+  }
+}
