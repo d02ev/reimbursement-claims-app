347febc71cc70402719d00798c9f0ae0f22e11e2
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-13 11:16:57 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import * as ExpressSession from 'express-session';
+import * as Passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = new ConfigService();
 
+  app.use(
+    ExpressSession({
+      secret: configService.get('SESSION_SECRET_KEY'),
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+      },
+    }),
+  );
+  app.use(Passport.initialize());
+  app.use(Passport.session());
+
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
