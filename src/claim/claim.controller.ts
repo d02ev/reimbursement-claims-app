759638e405a7e7eaae250cb8c51b178a81434a0d
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-14 12:37:24 +0530

    update claim module

--- src/claim/claim.controller.ts
+++ src/claim/claim.controller.ts
@@ -5,11 +5,20 @@ import {
   UseInterceptors,
   Body,
   UploadedFile,
+  Get,
+  Param,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 import { GetUser, UseRole } from 'src/auth/decorators';
 import { AuthenticatedGuard } from 'src/auth/guards';
 import { Role } from 'src/user/enum';
-import { CreateClaimDto } from './dto';
+import {
+  ApproveClaimDto,
+  CreateClaimDto,
+  DeclineClaimDto,
+  EditClaimDto,
+} from './dto';
 import { extname } from 'path';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
@@ -43,5 +52,115 @@ export class ClaimController {
     claimData.receipt = receiptImg.path;
     claimData.requestedBy = userId;
     return await this._claimService.generateClaim(claimData);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.ADMIN)
+  @Get()
+  public async getAllClaims(): Promise<any> {
+    return await this._claimService.getAllClaims();
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('user')
+  public async getAllUserClaims(@GetUser('id') userId: string): Promise<any> {
+    return await this._claimService.getClaimByUser(userId);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get(':claimId')
+  public async getClaimById(
+    @Param('claimId') claimId: string,
+    @GetUser('id') userId: string,
+  ): Promise<any> {
+    return await this._claimService.getClaimById(claimId, userId);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.ADMIN)
+  @Get('pending')
+  public async getAllPendingClaims(): Promise<any> {
+    return await this._claimService.getAllPendingClaims();
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.ADMIN)
+  @Get('approved')
+  public async getAllApprovedClaims(): Promise<any> {
+    return await this._claimService.getAllApprovedClaims();
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.ADMIN)
+  @Get('declined')
+  public async getAllDeclinedClaims(): Promise<any> {
+    return await this._claimService.getAllDeclinedClaims();
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.ADMIN)
+  @Patch('approve/:claimId')
+  public async approveClaim(
+    @Param('claimId') claimId: string,
+    @Body() approvingData: ApproveClaimDto,
+    @GetUser('email') userEmail: string,
+  ): Promise<any> {
+    approvingData.approvedBy = userEmail;
+    return await this._claimService.approveClaim(claimId, approvingData);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.ADMIN)
+  @Patch('decline/:claimId')
+  public async declineClaim(
+    @Param('claimdId') claimId: string,
+    @Body() decliningData: DeclineClaimDto,
+    @GetUser('email') userEmail: string,
+  ): Promise<any> {
+    decliningData.declinedBy = userEmail;
+    return await this._claimService.declineClaim(claimId, decliningData);
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.USER)
+  @Patch('edit/:claimId')
+  @UseInterceptors(
+    FileInterceptor('receipt', {
+      storage: diskStorage({
+        destination: './client/src/assets/receipts',
+        filename: (req, file, cb) => {
+          return cb(
+            null,
+            `${Date.now()}_${file.fieldname}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  public async editClaim(
+    @Param('claimId') claimId: string,
+    @UploadedFile() receiptImg: Express.Multer.File,
+    @GetUser('id') userId: string,
+    @Body() updatedClaimData: EditClaimDto,
+  ): Promise<any> {
+    if (receiptImg) {
+      updatedClaimData.receipt = receiptImg.path;
+    }
+
+    return await this._claimService.editClaim(
+      claimId,
+      userId,
+      updatedClaimData,
+    );
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @UseRole(Role.USER)
+  @Delete('delete/:claimId')
+  public async deleteClaim(
+    @Param('claimId') claimId: string,
+    @GetUser('id') userId: string,
+  ): Promise<any> {
+    return this._claimService.deleteClaim(claimId, userId);
   }
 }
--- src/claim/claim.service.ts
+++ src/claim/claim.service.ts
@@ -1,6 +1,13 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
-import { BaseClaimDto, CreateClaimDto } from './dto';
+import {
+  ApproveClaimDto,
+  BaseClaimDto,
+  CreateClaimDto,
+  DeclineClaimDto,
+  EditClaimDto,
+} from './dto';
+import { RequestPhase } from './enum';
 
 @Injectable()
 export class ClaimService {
@@ -29,6 +36,246 @@ export class ClaimService {
     return {
       status: 201,
       message: 'Claim Generated Successfully!',
+    };
+  }
+
+  public async getAllClaims(): Promise<any> {
+    const claims = await this._databaseService.claim.findMany({
+      include: { user: true },
+    });
+
+    if (claims.length === 0) {
+      return {
+        status: 200,
+        message: 'No Claims Found!',
+      };
+    }
+
+    return claims;
+  }
+
+  public async getAllPendingClaims(): Promise<any> {
+    const pendingClaims = await this._databaseService.claim.findMany({
+      where: { requestPhase: RequestPhase.IN_PROCESS },
+      include: { user: true },
+    });
+
+    if (pendingClaims.length === 0) {
+      return {
+        status: 200,
+        message: 'No Pending Claims Found!',
+      };
+    }
+
+    return pendingClaims;
+  }
+
+  public async getAllApprovedClaims(): Promise<any> {
+    const approvedClaims = await this._databaseService.claim.findMany({
+      where: { isApproved: true },
+      include: { user: true },
+    });
+
+    if (approvedClaims.length === 0) {
+      return {
+        status: 200,
+        message: 'No Approved Claims Found!',
+      };
+    }
+
+    return approvedClaims;
+  }
+
+  public async getAllDeclinedClaims(): Promise<any> {
+    const declinedClaims = await this._databaseService.claim.findMany({
+      where: { isApproved: false },
+      include: { user: true },
+    });
+
+    if (declinedClaims.length === 0) {
+      return {
+        status: 200,
+        message: 'No Declined Claims Found!',
+      };
+    }
+
+    return declinedClaims;
+  }
+
+  public async getClaimById(claimId: string, userId: string): Promise<any> {
+    const claim = await this._databaseService.claim.findFirst({
+      where: { id: claimId },
+    });
+
+    if (!claim) {
+      throw new HttpException(
+        'Requested Claim Does Not Exist!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    if (claim.requestedBy !== userId) {
+      throw new HttpException(
+        'The Requested Resource Does Not Belong To You!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    return claim;
+  }
+
+  public async getClaimByUser(userId: string): Promise<any> {
+    const claims = await this._databaseService.claim.findMany({
+      where: { requestedBy: userId },
+    });
+
+    if (claims.length === 0) {
+      return {
+        status: 200,
+        message: 'No Claims Found For The User!',
+      };
+    }
+
+    return claims;
+  }
+
+  public async approveClaim(
+    claimId: string,
+    approvingData: ApproveClaimDto,
+  ): Promise<any> {
+    const claim = await this._databaseService.claim.findUnique({
+      where: { id: claimId },
+    });
+
+    if (!claim) {
+      throw new HttpException(
+        'Requested Claim Does Not Exist!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const approvedClaim = await this._databaseService.claim.update({
+      where: { id: claimId },
+      data: { ...approvingData },
+    });
+
+    if (!approvedClaim) {
+      throw new HttpException(
+        'Cannot Approve The Claim Due To An Unknown Error!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return {
+      status: 200,
+      message: 'Claim Has Been Approved Successfully!',
+    };
+  }
+
+  public async declineClaim(
+    claimId: string,
+    decliningData: DeclineClaimDto,
+  ): Promise<any> {
+    const claim = await this._databaseService.claim.findUnique({
+      where: { id: claimId },
+    });
+
+    if (!claim) {
+      throw new HttpException(
+        'Requested Claim Does Not Exist!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const declinedClaim = await this._databaseService.claim.update({
+      where: { id: claimId },
+      data: { ...decliningData },
+    });
+
+    if (!declinedClaim) {
+      throw new HttpException(
+        'Cannot Decline The Claim Due To An Unknown Error!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return {
+      status: 200,
+      message: 'Claim Has Been Declined Successfully!',
+    };
+  }
+
+  public async editClaim(
+    claimId: string,
+    userId: string,
+    modifiedData: EditClaimDto,
+  ): Promise<any> {
+    const claim = await this._databaseService.claim.findUnique({
+      where: { id: claimId },
+    });
+
+    if (!claim) {
+      throw new HttpException(
+        'Requested Claim Does Not Exist!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    if (claim.requestedBy !== userId) {
+      throw new HttpException(
+        'The Requested Resource Does Not Belong To You!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    const modifiedClaim = await this._databaseService.claim.update({
+      where: { id: claimId },
+      data: { ...modifiedData },
+    });
+
+    if (!modifiedClaim) {
+      throw new HttpException(
+        'Cannot Edit The Claim Due To An Unknown Error!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return {
+      status: 200,
+      message: 'Claim Has Been Modified Successfully!',
+    };
+  }
+
+  public async deleteClaim(claimId: string, userId: string): Promise<any> {
+    const claim = await this._databaseService.claim.findUnique({
+      where: { id: claimId },
+    });
+
+    if (!claim) {
+      throw new HttpException(
+        'Requested Claim Does Not Exist!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    if (claim.requestedBy !== userId) {
+      throw new HttpException(
+        'The Requested Resource Does Not Belong To You!',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    const deletedClaim = await this._databaseService.claim.delete({
+      where: { id: claimId },
+    });
+
+    if (!deletedClaim) {
+      throw new HttpException(
+        'Cannot Delete Claim Due To An Unknown Error!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return {
+      status: 200,
+      message: 'Claim Deleted Successfully!',
     };
   }
 }
