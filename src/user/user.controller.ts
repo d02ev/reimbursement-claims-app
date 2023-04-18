7f83955aa411980360265ad0a461687b73641eb6
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-18 22:17:27 +0530

    update user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { AuthenticatedGuard } from 'src/auth/guards';
+import { JwtGuard } from 'src/auth/guards';
 import { GetUser, UseRole } from 'src/auth/decorators';
 import { Role } from './enum';
 import { EditUserDto } from './dto';
@@ -9,62 +17,62 @@ import { EditUserDto } from './dto';
 export class UserController {
   constructor(private readonly _userService: UserService) {}
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Get()
   public async getAllUsers(): Promise<any> {
     return this._userService.getAllUsers();
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @Get(':userId')
   public async getMe(@Param('userId') userId: string): Promise<any> {
     return this._userService.getUserById(userId);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Get('admins/all')
   public async getAllAdmins(): Promise<any> {
     return this._userService.getAllAdmins();
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Get('approvers/all')
   public async getAllApprovers(): Promise<any> {
     return this._userService.getAllApprovers();
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Patch('grant/admin/:userId')
   public async makeAdmin(@Param('userId') userId: string): Promise<any> {
     return this._userService.makeAdmin(userId);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Patch('revoke/admin/:userId')
   public async revokeAdmin(@Param('userId') userId: string): Promise<any> {
     return this._userService.removeAdmin(userId);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Patch('make/approver/:userId')
   public async makeApprover(@Param('userId') userId: string): Promise<any> {
     return this._userService.makeApprover(userId);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.ADMIN])
   @Patch('remove/approver/:userId')
   public async removeApprover(@Param('userId') userId: string): Promise<any> {
     return this._userService.removeApprover(userId);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   @UseRole([Role.USER])
   @Patch(':userId')
   public async updateUser(
