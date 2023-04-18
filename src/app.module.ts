f15df5ebef5a5a1b5ec0cae12bbe5ad8eae327f5
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-18 22:17:46 +0530

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,18 @@ import { AuthModule } from './auth/auth.module';
 import { ClaimModule } from './claim/claim.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RoleGuard } from './auth/guards';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd() + '/client/dist/client'),
+    }),
+    JwtModule.register({ global: true }),
+    UserModule,
     DatabaseModule,
     AuthModule,
     ClaimModule,
