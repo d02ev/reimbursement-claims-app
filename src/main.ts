075313633183ffd3d41c2903921595c40918f7ba
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-18 22:18:04 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import * as ExpressSession from 'express-session';
-import * as Passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = new ConfigService();
-
-  app.use(
-    ExpressSession({
-      secret: configService.get('SESSION_SECRET_KEY'),
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-      },
-    }),
-  );
-  app.use(Passport.initialize());
-  app.use(Passport.session());
 
   app.enableCors();
   app.setGlobalPrefix('api/v1');
