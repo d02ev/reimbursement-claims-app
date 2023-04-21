27c47b0e71e773455736a8e1494590044c492866
d02ev <vikramadityapratapsingh98@gmail.com>
2023-04-21 22:43:34 +0530

    udpate user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,62 +18,62 @@ export class UserController {
   constructor(private readonly _userService: UserService) {}
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Get()
   public async getAllUsers(): Promise<any> {
     return this._userService.getAllUsers();
   }
 
   @UseGuards(JwtGuard)
-  @Get(':userId')
-  public async getMe(@Param('userId') userId: string): Promise<any> {
+  @Get('me')
+  public async getMe(@GetUser('id') userId: string): Promise<any> {
     return this._userService.getUserById(userId);
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Get('admins/all')
   public async getAllAdmins(): Promise<any> {
     return this._userService.getAllAdmins();
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Get('approvers/all')
   public async getAllApprovers(): Promise<any> {
     return this._userService.getAllApprovers();
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Patch('grant/admin/:userId')
   public async makeAdmin(@Param('userId') userId: string): Promise<any> {
     return this._userService.makeAdmin(userId);
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Patch('revoke/admin/:userId')
   public async revokeAdmin(@Param('userId') userId: string): Promise<any> {
     return this._userService.removeAdmin(userId);
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Patch('make/approver/:userId')
   public async makeApprover(@Param('userId') userId: string): Promise<any> {
     return this._userService.makeApprover(userId);
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.ADMIN])
+  @UseRole(Role.ADMIN)
   @Patch('remove/approver/:userId')
   public async removeApprover(@Param('userId') userId: string): Promise<any> {
     return this._userService.removeApprover(userId);
   }
 
   @UseGuards(JwtGuard)
-  @UseRole([Role.USER])
+  @UseRole(Role.USER)
   @Patch(':userId')
   public async updateUser(
     @GetUser('id') userId: string,
