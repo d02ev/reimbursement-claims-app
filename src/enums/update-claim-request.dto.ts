121d6fc6631d6752f5b493d5a7cef48a5845386c
d02ev <vikramadityapratapsingh98@gmail.com>
2024-05-19 13:25:30 +0530

    minor updates

--- src/enums/update-claim-request.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { GenerateClaimRequestDto } from './generate-claim-request.dto';
-
-export class UpdateClaimRequestDto extends PartialType(
-	GenerateClaimRequestDto,
-) {
-	claimId: string;
-}
